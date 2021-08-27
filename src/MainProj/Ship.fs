552e0d0e525239971d4f8bf381845b6b186e2fe4
gretmn102 <erlgreywatson@gmail.com>
2021-08-27 04:41:42 +0300

    System.Drawing -> SixLabors.ImageSharp

--- src/MainProj/Ship.fs
+++ src/MainProj/Ship.fs
@@ -15,64 +15,70 @@ module WebClientDownloader =
         with
             | x -> Left x.Message
 
-open System.Drawing
-
+// #r @"C:\Users\User\.nuget\packages\sixlabors.fonts\1.0.0-beta15\lib\netstandard2.1\SixLabors.Fonts.dll"
+// #r @"C:\Users\User\.nuget\packages\sixlabors.imagesharp.drawing\1.0.0-beta13\lib\netcoreapp3.1\SixLabors.ImageSharp.Drawing.dll"
+// #r @"C:\Users\User\.nuget\packages\sixlabors.imagesharp\1.0.3\lib\netcoreapp3.1\SixLabors.ImageSharp.dll"
 // System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.PixelFormats
+open SixLabors.ImageSharp.Processing
+open SixLabors.ImageSharp.Drawing.Processing
 
 /// blue frame + heart + blue frame
 let (width, height), templateBmp =
     let path = @"heart.png"
     // let path = @"heart100.png"
-    use heart = new Bitmap(path)
-
+    use heart = Image.Load(path)
     let width, height = 133, 133 // heart.Width, heart.Height
-    let dstBmp = new Bitmap(width * 3, height)
+    let dstBmp = new Image<Rgba32>(width * 3, height)
 
-    use g = Graphics.FromImage dstBmp
-    g.DrawImage(heart, width, 0, width, height)
-    // g.DrawImageUnscaled(heart, 0, 0)
+    heart.Mutate (fun ctx ->
+        ctx.Resize(width, height)
+        |> ignore
+    )
+    dstBmp.Mutate (fun ctx ->
+        ctx.DrawImage(heart, Point(width, 0), new GraphicsOptions())
+        |> ignore
+    )
 
+    let widthf, heightf = float32 width, float32 height
     do
-        let c = Color.FromArgb 0xff2980b9
-        use p = new SolidBrush(c)
-        g.FillRectangle(p, 0, 0, width, height)
-        g.FillRectangle(p, width * 2, 0, width, height)
+        let c = Color.FromRgba(0x29uy, 0x80uy, 0xb9uy, 0xffuy)
+        dstBmp.Mutate(fun ctx ->
+            ctx.Fill(c, RectangleF(0.f, 0.f, widthf, heightf))
+            |> ignore
+            ctx.Fill(c, RectangleF(widthf * 2.f, 0.f, widthf, heightf))
+            |> ignore
+        )
 
     (width, height), dstBmp
+// templateBmp.Save("output.png")
 
 let img user1Avatar user2Avatar perc (outputStream:System.IO.MemoryStream) =
-    use dstBmp = new Bitmap(templateBmp.Width, templateBmp.Height)
-    use g = Graphics.FromImage dstBmp
-    g.DrawImageUnscaled(templateBmp, 0, 0)
+    use dstBmp = new Image<Rgba32>(templateBmp.Width, templateBmp.Height)
+    dstBmp.Mutate (fun ctx ->
+        ctx.DrawImage(templateBmp, new GraphicsOptions ())
+        |> ignore
+    )
 
     // [very cool gradient](https://stackoverflow.com/a/49321304)
     // but used [this](https://stackoverflow.com/a/2011839)
     let getColor i =
-        let max' = Color.FromArgb 0xff00ff00
-        let min' = Color.FromArgb 0xffff0000
-        let rMax = int max'.R
-        let rMin = int min'.R
-        let gMax = int max'.G
-        let gMin = int min'.G
-        let bMax = int max'.B
-        let bMin = int min'.B
+        let rMax, gMax, bMax = 0x00, 0xff, 00
+        let rMin, gMin, bMin = 0xff, 0x00, 0x00
 
         let i = float32 i
         let size = 100.0f // [0..100]
         let rAverage = rMin + int (float32 (rMax - rMin) * i / size)
         let gAverage = gMin + int (float32 (gMax - gMin) * i / size)
         let bAverage = bMin + int (float32 (bMax - bMin) * i / size)
-        Color.FromArgb(int rAverage, int gAverage, int bAverage)
+        Color.FromRgb(byte rAverage, byte gAverage, byte bAverage)
 
-    g.SmoothingMode <- Drawing2D.SmoothingMode.HighQuality
-    g.TextRenderingHint <- Text.TextRenderingHint.AntiAlias
-    g.InterpolationMode <- Drawing2D.InterpolationMode.HighQualityBicubic
-    g.PixelOffsetMode <- Drawing2D.PixelOffsetMode.HighQuality
-
-    let drawText shift i =
+    let drawText shift prec =
         // let i = 100
-        use txtBrush = new SolidBrush(getColor i)
-        use fontFamily = new FontFamily("DejaVu Sans Condensed")
+        let txtBrush = new SolidBrush(getColor prec)
+        // https://docs.sixlabors.com/articles/fonts/gettingstarted.html
+        let family = SixLabors.Fonts.SystemFonts.Find "DejaVu Sans Condensed"
         // FontFamily.Families
         // |> Array.find (fun x -> x.Name.Contains "eja")
 
@@ -82,36 +88,29 @@ let img user1Avatar user2Avatar perc (outputStream:System.IO.MemoryStream) =
             // (300. * x) / 320.
             (66.5f * float32 height) / 320.f
 
-        use font = new Font(fontFamily, fontSize)
-        let str = sprintf "%d%%" i
+        let font = new SixLabors.Fonts.Font(family, fontSize)
+        let str = sprintf "%d%%" prec
+
+        let options = new DrawingOptions()
+        let rendererOptions = new SixLabors.Fonts.RendererOptions(font, options.TextOptions.DpiX, options.TextOptions.DpiY)
         let size =
-            g.MeasureString(str, font, 0, StringFormat.GenericTypographic)
+            SixLabors.Fonts.TextMeasurer.Measure(str, rendererOptions);
 
         let srcWidth = size.Width
         let srcHeight = size.Height
         let x = float32 width * shift + (float32 width - srcWidth) / 2.f
         let y = (float32 height - srcHeight) / 2.f
-
-        let p = new Drawing2D.GraphicsPath()
-        p.AddString(
-            str,
-            fontFamily,
-            (int) FontStyle.Regular,
-            (g.DpiY * fontSize / 72.f),
-            new PointF(x, y),
-            StringFormat.GenericTypographic
+        let brush = //Brushes.Horizontal(Color.Red, Color.Blue)
+            txtBrush
+        let pen = Pens.Solid(Color.Black, (2.f * float32 height) / 320.f)
+        dstBmp.Mutate (fun ctx ->
+            ctx.DrawText(options, str, font, brush, pen, new PointF(x, y))
+            |> ignore
         )
-
-        use pen = Pens.Black.Clone() :?> Pen
-        pen.Width <-
-            (4.f * float32 height) / 320.f
-
-        g.DrawPath(pen, p);
-        g.FillPath(txtBrush, p)
 
     drawText 1.f perc
 
-    g.InterpolationMode <- Drawing2D.InterpolationMode.Bilinear
+    // g.InterpolationMode <- Drawing2D.InterpolationMode.Bilinear
 
     // 320 300
     // 133 x
@@ -119,8 +118,7 @@ let img user1Avatar user2Avatar perc (outputStream:System.IO.MemoryStream) =
     let size = (300.f * float32 height) / 320.f
 
     let f shift (xs:byte array) =
-        use imgStream = new System.IO.MemoryStream(xs)
-        use srcBmp = new Bitmap(imgStream)
+        use srcBmp = Image.Load(xs)
 
         let srcWidth = size
         let srcHeight = size
@@ -128,8 +126,14 @@ let img user1Avatar user2Avatar perc (outputStream:System.IO.MemoryStream) =
         let x = float32 width * shift + (float32 width - srcWidth) / 2.f
         let y = (float32 height - srcHeight) / 2.f
 
-        let r = RectangleF(x, y, srcWidth, srcHeight)
-        g.DrawImage(srcBmp, r)
+        srcBmp.Mutate (fun ctx ->
+            ctx.Resize(int srcWidth, int srcHeight)
+            |> ignore
+        )
+        dstBmp.Mutate (fun ctx ->
+            ctx.DrawImage(srcBmp, Point(int x, int y), new GraphicsOptions())
+            |> ignore
+        )
 
     user1Avatar
     |> Either.iter (f 0.f)
@@ -137,10 +141,12 @@ let img user1Avatar user2Avatar perc (outputStream:System.IO.MemoryStream) =
     user2Avatar
     |> Either.iter (f 2.f)
 
-    dstBmp.Save(outputStream, Imaging.ImageFormat.Png)
+    dstBmp.Save(outputStream, Formats.Png.PngFormat.Instance)
 
 // let user1Avatar = WebClientDownloader.getData [] "https://cdn.discordapp.com/avatars/796931597898088448/4aa0bed0799253a4bd5b119c355b28f1.png?size=128"
 // let user2Avatar = WebClientDownloader.getData [] "https://cdn.discordapp.com/avatars/436300487307034634/6112783a3ddbe8d59e6b6ec7f419b796.png?size=128"
+// #load "Ship.fs"
+// open Ship
 // do
 //     let m = new System.IO.MemoryStream()
 //     img user1Avatar user2Avatar 100 m
