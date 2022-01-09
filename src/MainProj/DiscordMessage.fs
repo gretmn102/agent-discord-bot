d056b2ad8bfa8a85971bdec90642cdf96f32e010
gretmn102 <erlgreywatson@gmail.com>
2022-01-09 17:08:25 +0200

    improve DiscordMessage.Parser functions

--- src/MainProj/DiscordMessage.fs
+++ src/MainProj/DiscordMessage.fs
@@ -15,7 +15,7 @@ module Parser =
         skipString "<@" >>. optional (skipChar '!') >>. puint64 .>> skipChar '>'
 
     let puserMentionTargetStr<'u> (userId: string) : Parser<_, 'u> =
-        skipString "<@" >>. optional (skipChar '!') >>. skipString userId >>. skipChar '>'
+        skipString "<@" >>? optional (skipChar '!') >>? skipString userId >>. skipChar '>'
 
     let puserMentionTarget<'u> (userId: UserId) : Parser<_, 'u> =
         puserMentionTargetStr (string userId)
@@ -24,7 +24,7 @@ module Parser =
         skipString "<@&" >>. puint64 .>> skipChar '>'
 
     let pmentionRoleTargetStr<'u> (roleId: string): Parser<_, 'u> =
-        skipString "<@&" >>. skipString roleId .>> skipChar '>'
+        skipString "<@&" >>? skipString roleId .>> skipChar '>'
 
     let pmentionRoleTarget<'u> (roleId: RoleId): Parser<_, 'u> =
         pmentionRoleTargetStr (string roleId)
@@ -32,11 +32,11 @@ module Parser =
     let pchannelMention<'u> : Parser<ChannelId, 'u> =
         skipString "<#" >>. puint64 .>> skipChar '>'
 
-    let pchannelMentionTargetRaw<'u> (channelId: string): Parser<_, 'u> =
-        skipString "<#" >>. skipString channelId .>> skipChar '>'
+    let pchannelMentionTargetStr<'u> (channelId: string): Parser<_, 'u> =
+        skipString "<#" >>? skipString channelId .>> skipChar '>'
 
     let pchannelMentionTarget<'u> (channelId: ChannelId): Parser<_, 'u> =
-        pchannelMentionTargetRaw (string channelId)
+        pchannelMentionTargetStr (string channelId)
 
     let pcustomEmoji<'u> : Parser<_, 'u> =
         pipe3
