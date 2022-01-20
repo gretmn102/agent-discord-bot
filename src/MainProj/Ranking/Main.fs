c5b87ec7038561df1e8a5ff84768a2fe66849910
gretmn102 <erlgreywatson@gmail.com>
2022-01-20 09:22:55 +0200

    ranking: set, get level roles

--- src/MainProj/Ranking/Main.fs
+++ src/MainProj/Ranking/Main.fs
@@ -7,6 +7,8 @@ open Model
 type SettingMsg =
     | SetOutputChannel of ChannelId
     | SetExp of UserId * uint64
+    | SetLevelRoles of (ChannelId * int) list
+    | GetLevelRoles
 
 type Msg =
     | NewMessageHandle of EventArgs.MessageCreateEventArgs
@@ -58,16 +60,73 @@ let updateExp
             let newLevel = getLevelBySumExp newExp
 
             if newLevel > 0UL && oldLevel <> newLevel then
+                let foundRole =
+                    setting.LevelRoles
+                    |> Array.tryPick (fun levelRole ->
+                        if levelRole.Level = int newLevel then
+                            match e.Guild.Roles.[levelRole.Role] with
+                            | null -> None
+                            | role -> Some role
+                        else
+                            None
+                    )
+
+                match foundRole with
+                | None -> ()
+                | Some role ->
+                    try
+                        let guildMember = await (e.Guild.GetMemberAsync userId)
+
+                        guildMember.GrantRoleAsync(role)
+                        |> fun x -> x.GetAwaiter() |> fun x -> x.GetResult()
+
+                        guildMember.Roles
+                        |> Array.ofSeq // because RevokeRoleAsync may invoke "Collection was modified; enumeration operation may not execute."
+                        |> Seq.iter (fun currRole ->
+                            let currRoleId = currRole.Id
+                            if currRoleId <> role.Id
+                               && setting.LevelRoles |> Array.exists (fun x -> currRole.Id = x.Role)
+
+                            then
+                                try
+                                    guildMember.RevokeRoleAsync currRole
+                                    |> fun x -> x.GetAwaiter() |> fun x -> x.GetResult()
+                                with e ->
+                                    printfn "2 %A" e.Message
+                        )
+                    with e ->
+                        printfn "1 %A" e.Message
+
                 match setting.OutputChannelId with
                 | Some outputChannelId ->
                     match e.Guild.GetChannel outputChannelId with
                     | null -> ()
                     | outputChannel ->
-                        let msg = sprintf "%s поднялся до %d уровня." e.Author.Username newLevel
-                        awaiti (outputChannel.SendMessageAsync(msg))
-                | None -> ()
+                        let msg =
+                            match foundRole with
+                            | None ->
+                                sprintf "<@!%d> поднялся до %d уровня." e.Author.Id newLevel
+                            | Some role ->
+                                sprintf "<@!%d> поднялся до %d уровня и получил роль <@&%d>." e.Author.Id newLevel role.Id
 
-                // TODO: дать роль, если она подошла по условиям
+                        let b = Entities.DiscordMessageBuilder()
+
+                        let color =
+                            match foundRole with
+                            | None -> Entities.DiscordColor "#2f3136"
+                            | Some x -> x.Color
+
+                        let embed =
+                            Entities.DiscordEmbedBuilder()
+                                .WithDescription(msg)
+                                .WithColor(color)
+                                .Build()
+
+                        b.WithEmbed embed |> ignore
+
+                        awaiti (outputChannel.SendMessageAsync(b))
+
+                | None -> ()
 
             let newRank =
                 { rank with Exp = newExp }
@@ -162,6 +221,77 @@ let settingReduce
 
             state
 
+    | SetLevelRoles levelRoles ->
+        let guild = e.Guild
+        let currentMember = await (guild.GetMemberAsync(e.Author.Id))
+        let replyMessage =
+            await (e.Channel.SendMessageAsync("Processing..."))
+
+        if currentMember.Permissions &&& Permissions.Administrator = Permissions.Administrator then
+            let levelRoles =
+                List.sort levelRoles
+                |> List.map (fun (roleId, level) ->
+                    { Role = roleId; Level = level } : RankingSettings.LevelRole
+                )
+                |> Array.ofList
+
+            let settings: RankingSettings.GuildRankingSettings =
+                let settings = state.Settings
+
+                match Map.tryFind e.Guild.Id settings with
+                | Some setting ->
+                    let setting =
+                        { setting with
+                            LevelRoles = levelRoles
+                        }
+
+                    RankingSettings.replace setting
+
+                    Map.add guild.Id setting settings
+                | None ->
+                    let x = RankingSettings.insert (guild.Id, levelRoles, None)
+                    Map.add guild.Id x settings
+
+            awaiti (replyMessage.ModifyAsync(Entities.Optional("Roles level has been set")))
+
+            { state with Settings = settings}
+
+        else
+            awaiti (replyMessage.ModifyAsync(Entities.Optional("You don't have permission")))
+
+            state
+
+    | GetLevelRoles ->
+        let guild = e.Guild
+        let replyMessage =
+            await (e.Channel.SendMessageAsync("Processing..."))
+
+        match Map.tryFind guild.Id state.Settings with
+        | Some setting ->
+            let msg =
+                setting.LevelRoles
+                |> Array.map (fun x -> sprintf "<@&%d> — %d" x.Role x.Level)
+                |> String.concat "\n"
+
+            let b = Entities.DiscordMessageBuilder()
+
+            let color = Entities.DiscordColor "#2f3136"
+
+            let embed =
+                Entities.DiscordEmbedBuilder()
+                    .WithDescription(msg)
+                    .WithColor(color)
+                    .Build()
+
+            b.WithEmbed embed |> ignore
+
+            awaiti (replyMessage.ModifyAsync(b))
+        | None ->
+            awaiti (replyMessage.ModifyAsync(Entities.Optional "Firstly set up the ranking system"))
+
+        state
+
+
 let reduce (msg: Msg) (state: State): State =
     match msg with
     | NewMessageHandle e ->
@@ -227,8 +357,22 @@ module Parser =
                 ((puserMention <|> puint64) .>> spaces)
                 puint64
 
+    let psetLevelRoles: _ Parser =
+        let proleIdLevel =
+            tuple2
+                ((pchannelMention <|> puint64) .>> spaces)
+                pint32
+
+        skipStringCI "setLevelRoles" >>. spaces
+        >>. many (proleIdLevel .>> spaces)
+
+    let pgetLevelRoles: _ Parser =
+        skipStringCI "getLevelRoles"
+
     let start: _ Parser =
         choice [
             psetOutputChannel |>> SetOutputChannel
             psetExp |>> SetExp
+            psetLevelRoles |>> SetLevelRoles
+            pgetLevelRoles >>% GetLevelRoles
         ]
