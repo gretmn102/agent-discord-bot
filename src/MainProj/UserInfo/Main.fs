4dc3b97844c2f5a73496738db7f98a248276a536
gretmn102 <erlgreywatson@gmail.com>
2022-04-26 23:40:35 +0300

    feat: get guild info

--- src/MainProj/UserInfo/Main.fs
+++ src/MainProj/UserInfo/Main.fs
@@ -7,6 +7,7 @@ open Types
 type Request =
     | GetAvatar of UserId option
     | GetUserInfo of UserId option
+    | GetGuildInfo of GuildId option
 
 module Parser =
     open FParsec
@@ -23,10 +24,15 @@ module Parser =
         skipStringCI "user" >>. spaces
         >>. opt (puserMention <|> puint64)
 
+    let pgetGuildInfo: _ Parser =
+        skipStringCI "guild" >>. spaces
+        >>. opt puint64
+
     let start: _ Parser =
         choice [
             pgetAvatar |>> GetAvatar
             pgetUserInfo |>> GetUserInfo
+            pgetGuildInfo |>> GetGuildInfo
         ]
 
 let exec (client: DiscordClient) (e: EventArgs.MessageCreateEventArgs) (msg: Request) =
@@ -159,5 +165,103 @@ let exec (client: DiscordClient) (e: EventArgs.MessageCreateEventArgs) (msg: Req
                     b
                 | user -> createMessage user
             | None -> createMessage e.Author
+
+        ignore (await (e.Channel.SendMessageAsync message))
+
+    | GetGuildInfo otherGuildIdOrSelf ->
+        e.Channel.TriggerTypingAsync().GetAwaiter().GetResult()
+
+        let message =
+            let createMessage (guild: Entities.DiscordGuild) =
+                let embed =
+                    let fields =
+                        [
+                            let membersCount, botsCount =
+                                guild.Members
+                                |> Seq.fold
+                                    (fun (membersCount, botsCount) x ->
+                                        if x.Value.IsBot then
+                                            membersCount, botsCount + 1
+                                        else
+                                            membersCount + 1, botsCount
+                                    )
+                                    (0, 0)
+                            "Участники:", [
+                                sprintf "Всего: **%d**" guild.MemberCount
+                                sprintf "Людей: **%d**" membersCount
+                                sprintf "Ботов: **%d**" botsCount
+                            ] |> String.concat "\n"
+
+                            let statuses =
+                                let m =
+                                    guild.Members
+                                    |> Seq.fold
+                                        (fun m (KeyValue(_, user)) ->
+                                            match user.Presence with
+                                            | null ->
+                                                m
+                                                |> Map.addOrModWith
+                                                    Entities.UserStatus.Offline
+                                                    (fun () -> 1)
+                                                    (fun acc -> acc + 1)
+
+                                            | presence ->
+                                                m
+                                                |> Map.addOrModWith
+                                                    presence.Status
+                                                    (fun () -> 1)
+                                                    (fun acc -> acc + 1)
+                                        )
+                                        Map.empty
+
+                                let f status = Map.tryFind status m |> Option.defaultValue 0
+                                [
+                                    sprintf "<:online:636551903299371008> В сети: **%d**" (f Entities.UserStatus.Online)
+                                    sprintf "<:idle:636551903123210269> Нет на месте: **%d**" (f Entities.UserStatus.Idle)
+                                    sprintf "<:dnd:636551902716362764> Не беспокоить: **%d**" (f Entities.UserStatus.DoNotDisturb)
+                                    sprintf "<:offline:636551904096157697> Не в сети: **%d**" (f Entities.UserStatus.Offline)
+                                ] |> String.concat "\n"
+                            "По статусам:", statuses
+
+                            "Владелец:", sprintf "<@!%d>" guild.OwnerId
+
+                            let dateTime = DateTime.Unix.toSec guild.CreationTimestamp.UtcDateTime
+                            "Дата создания:", sprintf "<t:%d:D> (<t:%d:R>)" dateTime dateTime
+                        ]
+
+                    let embed =
+                        Entities.DiscordEmbedBuilder()
+                            .WithColor(Entities.DiscordColor "#2f3136")
+                            .WithThumbnail(guild.IconUrl)
+                            .WithImageUrl(guild.BannerUrl)
+
+                    let embed =
+                        fields
+                        |> List.fold
+                            (fun (b: Entities.DiscordEmbedBuilder) (header, value) ->
+                                b.AddField(header, value, true)
+                            )
+                            embed
+
+                    embed.Build()
+
+                let b = Entities.DiscordMessageBuilder()
+                b.Embed <- embed
+                b
+
+            match otherGuildIdOrSelf with
+            | Some guildId ->
+                let guild =
+                    try
+                        await (client.GetGuildAsync guildId)
+                    with e -> null
+
+                match guild with
+                | null ->
+                    let b = Entities.DiscordMessageBuilder()
+                    b.Content <- sprintf "Guild %d not found" guildId
+                    b
+                | guild -> createMessage guild
+            | None -> createMessage e.Guild
 
         ignore (await (e.Channel.SendMessageAsync message))
