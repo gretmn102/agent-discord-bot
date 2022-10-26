6250ea8df62391ce45d2b03ae3815489fca06275
gretmn102 <erlgreywatson@gmail.com>
2022-10-27 02:29:54 +0300

    customCommand: add kebab

--- src/MainProj/CustomCommand/Main.fs
+++ src/MainProj/CustomCommand/Main.fs
@@ -16,6 +16,7 @@ type Act =
     | Battery
     | Catch
     | Angry
+    | Kebab
     | FortuneCookies
 
 type Msg = Act * UserId option
@@ -38,6 +39,7 @@ module Parser =
             skipStringCI "catch" >>% Catch
             skipStringCI "angry" >>% Angry
             skipStringCI "печенье" >>% FortuneCookies
+            (skipStringCI "шашлычок" <|> skipStringCI "шашлык") >>% Kebab
         ]
 
     let start: Msg Parser =
@@ -144,6 +146,17 @@ let angryAssets =
         "https://c.tenor.com/Hn87zvoPNkAAAAAC/fire-bear.gif"
     |]
 
+let kebabs =
+    [|
+        "https://media.tenor.com/LtTjLfArzqAAAAAd/sheftalia-karvouna.gif"
+        "https://media.tenor.com/Xufgg6gSRPkAAAAC/%E1%83%9B%E1%83%AC%E1%83%95%E1%83%90%E1%83%93%E1%83%98-mwvadi.gif"
+        "https://media.tenor.com/ayzVsly9VGEAAAAd/%E1%83%90%E1%83%97%E1%83%9A%E1%83%94%E1%83%97%E1%83%98%E1%83%99%E1%83%90%E1%83%AA%E1%83%98-%E1%83%9B%E1%83%94%E1%83%A5%E1%83%90%E1%83%91%E1%83%90%E1%83%91%E1%83%94%E1%83%AF%E1%83%94%E1%83%9A%E1%83%90%E1%83%9A%E1%83%98.gif"
+        "https://media.tenor.com/_ZQHECcQ1nsAAAAd/%D1%88%D0%B0%D1%88%D0%BB%D1%8B%D0%BA-%D0%B1%D0%B5%D0%B1%D1%80%D0%B0.gif"
+        "https://media.tenor.com/qeU8IezNdJIAAAAS/%EC%8B%9D%ED%92%88%EA%B0%80%EA%B3%B5.gif"
+        "https://media.tenor.com/1jIM6pxDDdMAAAAC/bbq-meat.gif"
+        "https://media.tenor.com/RdEYFJcw8BYAAAAC/brasero.gif"
+    |]
+
 let exec (client: DiscordClient) (e: EventArgs.MessageCreateEventArgs) ((msg, whomId): Msg) =
     let cmdBuilder = cmdBuilder client e
 
@@ -204,6 +217,66 @@ let exec (client: DiscordClient) (e: EventArgs.MessageCreateEventArgs) ((msg, wh
             (sprintf "**%s** злится на **%s**")
             "На самого себя нельзя злиться, ну в самом деле!"
             "На меня не надо злиться, я хороший!"
+    | Kebab ->
+        let predictions = kebabs
+
+        let send whomId =
+            let whom =
+                whomId
+                |> Option.map (fun whomId ->
+                    try
+                        await (client.GetUserAsync whomId)
+                        |> Some
+                        |> Ok
+                    with e ->
+                        sprintf "Пользователя %d не существует" whomId
+                        |> Error
+                )
+                |> Option.defaultValue (Ok None)
+
+            let mkContent () =
+                match whom with
+                | Ok whom ->
+                    match whom with
+                    | None ->
+                        sprintf "<@%d> жарит себе шашлычок."
+                            e.Author.Id
+                    | Some whom ->
+                        let authorId = e.Author.Id
+                        let botId = client.CurrentUser.Id
+
+                        if whom.Id = authorId then
+                            sprintf "<@%d> жарит себе шашлычок."
+                                authorId
+                        elif whom.Id = botId then
+                            sprintf "<@%d> жарит мне шашлычок, пасеба 😋"
+                                authorId
+                        else
+                            sprintf "<@%d> делает шашлычок для <@%d>."
+                                authorId
+                                whom.Id
+                | Error errMsg -> errMsg
+
+            let prediction =
+                predictions.[r.Next(0, predictions.Length)]
+
+            let b = Entities.DiscordEmbedBuilder()
+            b.Description <- mkContent ()
+            b.Color <- Entities.Optional.FromValue(DiscordEmbed.backgroundColorDarkTheme)
+            b.ImageUrl <- prediction
+
+            awaiti (client.SendMessageAsync (e.Channel, b.Build()))
+
+        match whomId with
+        | Some whomId ->
+            send (Some whomId)
+        | None ->
+            match e.Message.ReferencedMessage with
+            | null ->
+                send whomId
+            | referencedMessage ->
+                send (Some referencedMessage.Author.Id)
+
     | FortuneCookies ->
         let predictions =
             [|
