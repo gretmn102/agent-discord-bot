ffb7f90bb113398070f64ed578a7669548782fa5
gretmn102 <erlgreywatson@gmail.com>
2022-12-13 10:31:46 +0200

    refact: move Shared to DiscordBotExtensions

--- tests/ApiTests.fs
+++ tests/ApiTests.fs
@@ -6,30 +6,6 @@ open IO.Ably.Realtime
 open Types
 open Shared.Api
 
-[<Tests>]
-let snowflakeTests =
-    testList "snowflakeTests" [
-        testCase "serialize and deserialize" <| fun () ->
-            let converter = SnowflakeConverter()
-
-            let ser x =
-                Newtonsoft.Json.JsonConvert.SerializeObject(x, converter)
-
-            let des (str: string) =
-                Newtonsoft.Json.JsonConvert.DeserializeObject(str, converter)
-
-            let sample = Snowflake.Create 12345UL
-
-            let exp = "\"12345\""
-            let act = ser sample
-
-            Assert.Equal("", exp, act)
-
-            let act: Snowflake = des exp
-
-            Assert.Equal("", sample, act)
-    ]
-
 module Doorkeeper =
     module ApiTests =
         open Doorkeeper.Api.TransferTypes
