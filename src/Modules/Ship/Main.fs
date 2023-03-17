f1851d99eeeda25309525baa771ccaff70128282
gretmn102 <erlgreywatson@gmail.com>
2023-03-17 23:03:18 +0200

    ship: add descriptions

--- src/Modules/Ship/Main.fs
+++ src/Modules/Ship/Main.fs
@@ -47,6 +47,82 @@ module Parser =
 
 let r = System.Random ()
 
+let binarySearch compare length =
+    let rec f from to' =
+        let diff = to' - from
+
+        if diff = 0 then
+            let res = compare from
+            if res = 0 then
+                Choice1Of2 from
+            elif res > 0 then
+                Choice2Of2 (from - 1, from)
+            else
+                Choice2Of2 (from, from + 1)
+        elif diff = 1 then
+            let res = compare from
+            if res = 0 then
+                Choice1Of2 from
+            elif res < 0 then
+                let from = from + 1
+                let res = compare from
+                if res = 0 then
+                    Choice1Of2 from
+                elif res > 0 then
+                    Choice2Of2 (from - 1, from)
+                else
+                    Choice2Of2 (from, from + 1)
+            else
+                Choice2Of2 (from - 1, from)
+        else
+            let average = diff / 2
+            let idx = from + average
+            let res = compare idx
+            if res = 0 then
+                Choice1Of2 idx
+            elif res > 0 then
+                f from (idx - 1)
+            else
+                f (idx + 1) to'
+
+    if length > 0 then
+        f 0 (length - 1)
+    else
+        invalidArg "length" "Length must be greater then 0"
+
+let getDescription =
+    let results =
+        [|
+            0  , "Ничто в мире не даётся без труда — даже любовь..."
+            9  , "Ну, ничего, будет и на вашей улице свадьба... Но не сегодня."
+            19 , "Любовь зла... как хорошо, что её тут нет!"
+            29 , "Сейчас посмотрим Ваш диагноз. Ну, среди всего любви точно нет."
+            39 , "Вы испытываете чувства, но не стараетесь! Давай попробуй ещё раз, я в вас верю!"
+            49 , "Воздух переполнен страстью, витающей в нём. Задыхаюсь! 😱"
+            59 , "Чистые и искренние чувства, которые ещё предстоит проверить на прочность."
+            69 , "Кажется, вырисовываются жених и невеста!"
+            79 , "Влюбиться — влюбились, осталось взрастить любовь..."
+            89 , "Если бы любовь можно было бы измерить деньгами, эти двое были бы уже миллионерами."
+            99 , "За такие чувства не жалко и умереть!"
+            100, "Дошипперились? Теперь женитесь и живите долго и счастливо"
+        |]
+        |> Array.sortBy fst
+
+    fun procent ->
+        let res =
+            results.Length
+            |> binarySearch (fun idx ->
+                let item, _ = results.[idx]
+                item.CompareTo procent
+            )
+        match res with
+        | Choice1Of2 idx ->
+            results.[idx]
+        | Choice2Of2(lb, ub) ->
+            let ub = if ub < results.Length then ub else results.Length - 1
+            results.[ub]
+        |> snd
+
 let cmdBuilder2
     (e: EventArgs.MessageCreateEventArgs)
     (botId: UserId)
@@ -68,15 +144,7 @@ let cmdBuilder2
         let embed = Entities.DiscordEmbedBuilder()
         embed.Color <- Entities.Optional.FromValue(DiscordEmbed.backgroundColorDarkTheme)
         embed.Description <-
-            let nickOrName (user:Entities.DiscordMember) =
-                match user.Nickname with
-                | null -> user.Username
-                | nick -> nick
-
-            if perc < 50 then
-                sprintf "Между %s и %s..." (nickOrName who) (nickOrName whom)
-            else
-                sprintf "Между %s и %s что-то есть!" (nickOrName who) (nickOrName whom)
+            getDescription perc
 
         let imageName = "ship.png"
 
